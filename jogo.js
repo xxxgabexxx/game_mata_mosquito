b7492c8288ade19c4f90bbc773ad9a3abaefd81c
Gabriela Zanette <gabezanette.dev@gmail.com>
2023-07-16 19:08:43 -0300

    Criando posicoes randomicas

--- jogo.js
+++ jogo.js
@@ -1,10 +1,35 @@
+
+//ajustando a dimensão do palco
 var altura  = 0
 var largura = 0
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura);
+    console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
+// fim da dimensão do palco
+
+
+// posição randomica do mosquito
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90 
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+    
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+    
+    //criando o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
